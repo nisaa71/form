4a4a66accd8223c05eecfd289db61e1efa130d85
nisaa71 <nisaa71@gmail.com>
2020-03-02 10:44:58 +0700

    halaman login

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<center><h2>Welcome to Administrator</h2>
+	<form action="proseslogin.php" method="post">
+	<td>Username</td><input type="text" name="username" id=""><br>
+	<td>Password</td><input type="password" name="password" id=""><br>
+	<input type="submit" name="" id="">
+	</form>
+	</center>
+</body>
+</html>
